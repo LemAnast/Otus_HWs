985e7d87eb380d1b4cdae9e1f8090f6e2e10df51
Lemanast <lemanast@yandex.ru>
2024-01-14 20:45:48 +0300

    Edited the methods

--- HW_2/src/circle.py
+++ HW_2/src/circle.py
@@ -1,10 +1,17 @@
+from math import pi
 from HW_2.src.figure import Figure
 
 
 class Circle(Figure):
 
-    def __init__(self, side_a: int):
-        super().__init__(side_a)
-        if side_a <= 0:
+    def __init__(self, radius: int):
+        if radius <= 0:
             raise ValueError("Радиус круга должен быть числом больше нуля")
+        super().__init__(name="Circle")
+        self.radius = radius
 
+    def get_area(self):
+        return pi * self.radius ** 2
+
+    def get_perimeter(self):
+        return 2 * pi * self.radius
