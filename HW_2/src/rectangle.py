96b11b20e646f1bfba802590179463839c15ba84
Lemanast <lemanast@yandex.ru>
2024-01-14 21:25:58 +0300

    Edited class Square

--- HW_2/src/rectangle.py
+++ HW_2/src/rectangle.py
@@ -4,9 +4,9 @@ from HW_2.src.figure import Figure
 class Rectangle(Figure):
 
     def __init__(self, side_a: int, side_b: int):
-        super().__init__(name="Rectangle")
         if side_a <= 0 or side_b <= 0:
             raise ValueError("Стороны прямоугольника должны быть числом больше нуля")
+        super().__init__(name="Rectangle")
         self.side_a = side_a
         self.side_b = side_b
 
